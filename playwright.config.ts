6a525bd965f714516b3392d53e2dbb3b21979dfe
Karol Goralczyk <goral44@gmail.com>
2026-05-09 10:46:19 +0200

    Updated GitHub workflow for envs and manual execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
